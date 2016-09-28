a083ff55bc8a8869a3ebd41bdfcbb19699c76248
Goldenson <orefice.maxime@gmail.com>
2016-09-27 20:58:07 -0400

    Add validations to uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,15 @@
 class ImageUploader < Shrine
-  # plugins and uploading logic
+  plugin :determine_mime_type
+  plugin :validation_helpers
   plugin :backgrounding
+
+  # Validations
+  Attacher.validate do
+    validate_mime_type_inclusion %w[image/jpeg image/gif image/png]
+    validate_max_size 2.megabytes, message: 'is too large (max is 2 MB)'
+  end
+
+  # Background
   Attacher.promote { |data| PromoteJob.set(wait: 5.seconds).perform_later(data) }
   Attacher.delete { |data| DeleteJob.perform_later(data) }
 end
